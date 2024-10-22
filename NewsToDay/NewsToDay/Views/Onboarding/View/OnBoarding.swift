b0ed7a0e509c7c887a394ce09c3863846cba736e
Ylyas Abdywahytow <ylyasabdywahytow@Ylyass-MacBook-Pro.local>
2024-10-21 22:27:01 -0400

    add DefaultButtonView and Sample Onboarding

--- NewsToDay/NewsToDay/Views/Onboarding/View/OnBoarding.swift
+++ NewsToDay/NewsToDay/Views/Onboarding/View/OnBoarding.swift
@@ -8,11 +8,76 @@
 import SwiftUI
 
 struct OnBoarding: View {
+    
     var body: some View {
-        Text("OnBoarding")
+        NavigationView{
+            VStack {
+                Spacer()
+                    .frame(height: 50)
+                
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        Image("square")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 288, height: 366)
+                        
+                        Image("city")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 288, height: 366)
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    .frame(maxWidth: .infinity)
+                }
+                .frame(height: 366)
+                
+                
+               customProgressView(activeIndex: 0, totalSteps: 3)
+                    .padding(.top, 20)
+                
+                
+                Text("First to know")
+                    .foregroundColor(.blackPrimary)
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .padding(.top, 20)
+                
+                Spacer()
+                Text("All news in one place, be\n the first to know last news")
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 20)
+                
+                Spacer()
+                DefaultButtonView()
+                    .padding(.trailing,2)
+                    .padding(.bottom, 20)
+            }
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
 #Preview {
     OnBoarding()
 }
+
+func customProgressView(activeIndex: Int, totalSteps: Int) -> some View {
+      HStack(spacing: 12) {
+          ForEach(0..<totalSteps, id: \.self) { index in
+              if index == activeIndex {
+                  
+                  Capsule()
+                      .fill(Color.blue)
+                      .frame(width: 24, height: 8)
+              } else {
+                
+                  Circle()
+                      .fill(Color.gray.opacity(0.2))
+                      .frame(width: 8, height: 8)
+              }
+          }
+      }
+  }
